dd65601c178ff580b6e5fb4c2bb040446a1756e9
Faizhan23 <Mohamedfaizhan460@gmail.com>
2026-02-22 17:25:57 +0530

    Create roles table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,12 @@ CREATE DATABASE ocean_view_resort
     COLLATE utf8mb4_unicode_ci;
 
 USE ocean_view_resort;
+
+
+CREATE TABLE roles (
+    role_id   TINYINT      NOT NULL AUTO_INCREMENT,
+    role_name VARCHAR(50)  NOT NULL,
+    PRIMARY KEY (role_id),
+    UNIQUE KEY uq_role_name (role_name)
+);
+
